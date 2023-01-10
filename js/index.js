fbc9cf20461133dbbd53b2965e6b90b86f41df50
Filipp Pinskiy <pinskiy.fs@ya.ru>
2023-01-10 19:18:43 +0300

    init dev

---
+++ js/index.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const title = document.querySelector('.form__title');
+const cardId = document.querySelector('.title--id');
+const btnId = document.querySelector('.title__btn--id');
+const form = document.querySelector('.form');
+const checkbox = document.querySelector('.checkbox__input');
+const inputCheckbox = document.querySelector('.form__input_disabled');
+const totalPrice = document.querySelector('.total-cost__text');
+
+
+
